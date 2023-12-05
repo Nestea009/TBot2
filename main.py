a8a7e76270919bb42d52408e4a3a5659c55d94ac
Néstor <nestorblancotech@gmail.com>
2023-12-05 15:07:58 +0100

    Moving averages progress

---
+++ main.py
@@ -0,0 +1,79 @@
+import requests
+import time
+import json
+
+API_KEY = "PKMRS0PD5QOPSB14455X"
+API_SECRET = "RooSe7SdHmP3vQB1cshk2LxHZ5vY2lbjDu7v5cWD"
+BASIC_URL = "https://paper-api.alpaca.markets/v2"
+symbol = "AAPL"
+last_trade_url = f"https://data.alpaca.markets/v2/stocks/{symbol}/trades/latest"
+
+headers = {
+  'APCA-API-KEY-ID': "PK42I07MH09F0ORJLLC1",
+  'APCA-API-SECRET-KEY': "3UQQh5eNtNRdlcUgespElmCD3JqW2iDOd6AiruZd"
+}
+
+def PlaceBuyAAPL(actual_price):
+  order_data = {
+      "symbol": "AAPL",
+      "qty": 1,
+      "side": "buy",
+      "type": "market",
+      "time_in_force": "day",
+  }
+
+  ORDER_URL = "https://paper-api.alpaca.markets/v2/orders"
+
+  r = requests.post(ORDER_URL, json=order_data, headers=headers)
+  
+  print(f"{order_data['qty']} {order_data['symbol']} stock(s) bought at {actual_price} each!")
+
+  return actual_price
+
+def PlaceSellAPPL(actual_price):
+  order_data = {
+      "symbol": "AAPL",
+      "qty": 1,
+      "side": "sell",
+      "type": "market",
+      "time_in_force": "day",
+  }
+
+  ORDER_URL = "https://paper-api.alpaca.markets/v2/orders"
+
+  r = requests.post(ORDER_URL, json=order_data, headers=headers)
+
+  print(order_data['qty'], order_data['symbol'], "stocks sold at", actual_price, "each!")
+
+  return actual_price
+
+def FindPrice():
+  current = requests.get(last_trade_url, headers=headers)
+  current_json = current.json()
+  current_price = current_json['trade']
+  actual_price = current_price['p']
+
+  return actual_price
+
+
+def Strategy():
+  url1 = "https://data.alpaca.markets/v2/stocks/AAPL/quotes?start=2023-12-01&end=2023-12-04&limit=100&feed=iex&sort=asc"
+
+
+  headers1 = {
+    "accept": "application/json",
+    "APCA-API-KEY-ID": "PK42I07MH09F0ORJLLC1",
+    "APCA-API-SECRET-KEY": "3UQQh5eNtNRdlcUgespElmCD3JqW2iDOd6AiruZd"
+}
+
+  response = requests.get(url1, headers=headers)
+
+  data = json.loads(response.text)
+
+  arr = []
+  for day in data["quotes"]:
+    arr.append(day["ap"])
+
+  print(arr)
+
+Strategy()
